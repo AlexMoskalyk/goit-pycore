e4eb2c18bb7147dc4ab6d84c2a4f10998e2e8a0c
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-11-23 15:55:51 +0100

    add task2

--- task1.py
+++ task1.py
@@ -2,17 +2,10 @@ from datetime import datetime
 
 
 
-def get_days_from_today(date):
-    # Convert the input date string to a datetime object
+def get_days_from_today(date:str)->int:
     input_date = datetime.strptime(date, '%Y-%m-%d')
-    
-    # Get the current date
     current_date = datetime.today()
-    
-    # Calculate the difference between the current date and the input date
     date_diff = current_date - input_date
-    
-    # Return the difference in days as an integer
     return date_diff.days
 
 print(get_days_from_today('2024-11-22'))
